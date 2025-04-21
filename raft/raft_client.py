26100c3a25163d4a22669c117ba30d1c0c54d5d7
m.dyachenko2 <m.dyachenko2@g.nsu.ru>
2025-04-21 13:44:41 +0000

    added some types

--- raft/raft_client.py
+++ raft/raft_client.py
@@ -1,6 +1,20 @@
 from abc import ABC, abstractsmethod
 
+from pydantic import BaseModel
+
+from enum import Enum
+
+
+class ResponseStatus(Enum):
+    FIAL = 'FAIL'
+    SUCCESS = 'SUCCESS'
+
+
+class ApplyCommandResponse(BaseModel):
+    status: ResponseStatus
+    body: dict | None
+
 
 class RaftClient(ABC):
     @abstractsmethod
-    async def apply(self, command: dict) -> dict: ...
+    async def apply(self, command: dict) -> ApplyCommandResponse: ...
